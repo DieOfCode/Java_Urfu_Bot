3b2ce3c7dc49ff1e9fd9b48821e4b43ec16a1e0c
DieOfCode <fr0ststone878@gmail.com>
2020-09-20 23:59:23 +0500

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        Scanner scan = new Scanner(System.in);
+
+        while (true){
+            var response = bot.messageHandler(scan.nextLine());
+            if (Boolean.parseBoolean(response))break;
+            else System.out.println(Boolean.parseBoolean(response));
+        }
+    }
+}
